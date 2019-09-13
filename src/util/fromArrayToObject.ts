53b89e8a8b6b1c9144f96c207c011fcf9ece440c
早津渉 <top.men09@gmail.com>
2019-09-13 18:37:01 +0900

    utilのリファクタリング

--- src/util/fromArrayToObject.ts
+++ src/util/fromArrayToObject.ts
@@ -3,7 +3,7 @@
 export const fromArrayToObject = <T extends Readonly<{ id: number }>>(
   items: ReadonlyArray<T>
 ): Readonly<{ [id: number]: T }> =>
-  items.reduce((map: any | {}, item) => {
+  items.reduce<{ [id: number]: T }>((map, item) => {
     map[item.id] = item
     return map
   }, {})
