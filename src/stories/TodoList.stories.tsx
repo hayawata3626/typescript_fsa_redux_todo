60d8601aec12d839edfd83a6de1c6fb964f3ab3e
早津渉 <top.men09@gmail.com>
2020-01-13 12:18:01 +0900

    storyのpropsのデータ作成にfactoryを

--- src/stories/TodoList.stories.tsx
+++ src/stories/TodoList.stories.tsx
@@ -4,17 +4,11 @@ import { TodoList } from "../component/TodoList"
 import { FilterType } from "../state/todoAppState"
 import { Provider } from "react-redux"
 import { store } from "../store"
+import { TodoItemFactory } from "../factory"
 
 storiesOf("TodoList", module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
   .add("通常", () => {
-    return (
-      <TodoList
-        todoList={[
-          { id: 1, title: "買い物へ行く", done: false, selected: false },
-          { id: 2, title: "銀行へ行く", done: false, selected: false }
-        ]}
-        filterType={FilterType.Active}
-      />
-    )
+    const taskList = TodoItemFactory.buildList(2)
+    return <TodoList todoList={taskList} filterType={FilterType.Active} />
   })
