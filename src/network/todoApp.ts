5e34ccef49cfed342ceefe3a4963a0ee6a541a30
早津渉 <top.men09@gmail.com>
2020-02-13 12:17:46 +0900

    使われていないfetch処理を消した

--- src/network/todoApp.ts
+++ src/network/todoApp.ts
@@ -2,10 +2,6 @@ import axios, { AxiosResponse } from "axios"
 
 const baseUrl = "http://localhost:4000"
 
-/* タスク候補取得用 */
-export const loadCandidateOfTodoList = (): Promise<AxiosResponse<any>> =>
-  axios.get(`${baseUrl}/todoList`)
-
 /* 初期データ読み込み用 */
 export const fetchInitialData = (): Promise<AxiosResponse<any>> =>
   axios.get(`${baseUrl}/todoList`)
